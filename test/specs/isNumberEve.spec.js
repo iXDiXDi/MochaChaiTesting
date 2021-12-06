df5d31ed8d4ecc6d13e0d056fa60403d8cbd66ba
Dmitriy Karpenko <ixdixdi@gmail.com>
2021-12-06 23:05:24 +0300

    1st

---
+++ test/specs/isNumberEve.spec.js
@@ -0,0 +1,22 @@
+/* eslint-disable linebreak-style */
+const NumbersValidator = require('../../app/numbers_validator');
+const {expect} = require('chai');
+
+describe(`isNumberEven positive scenarios`, function() {
+  let validator;
+  beforeEach(function() {
+    validator = new NumbersValidator();
+  });
+  afterEach(function() {
+    validator = null;
+  });
+  it(`should return true if number is even`, function() {
+    expect(validator.isNumberEven(4)).to.be.equal(true);
+  });
+  it(`should return false if number is odd`, function() {
+    expect(validator.isNumberEven(7)).to.be.equal(false);
+  });
+  it(`chai test`, function() {
+    expect(validator.isNumberEven(5)).to.be.equal(false);
+  });
+});
